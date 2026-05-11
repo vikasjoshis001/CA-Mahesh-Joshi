87d4bba3cca7402222f2ad8937d47a5ae06dbd1a
VJ <vikasjoshis001@gmail.com>
2026-05-11 20:45:28 +0530

    Add Google Tag Manager integration for enhanced tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { Header, Footer } from "@/components/layout";
 import { WhatsAppButton } from "@/components/WhatsAppButton";
 import { LocalBusinessSchema, PersonSchema } from "@/components/StructuredData";
 import { siteConfig } from "@/config/site";
 import { homeMetadata } from "@/lib/metadata";
+
+const GTM_ID = "GTM-K9SK2X9W";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -49,7 +52,27 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`}
+        </Script>
+      </head>
       <body className="min-h-full flex flex-col">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         {/* Structured Data for SEO */}
         <LocalBusinessSchema />
         <PersonSchema />
